357b4b5cc36646cf7318141231c39809d9eba2df
admin <thennavan.tech01@gmail.com>
2023-08-14 19:53:31 +0530

    First Commit

---
+++ student-management-system/src/main/java/com/example/sms/ServiceImpl/StudentServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.sms.ServiceImpl;
+import java.util.List;
+
+import com.example.sms.Entity.Student;
+import com.example.sms.Repository.StudentRepository;
+import com.example.sms.Service.StudentService;
+
+
+@org.springframework.stereotype.Service
+public class StudentServiceImpl implements StudentService{
+
+	private StudentRepository studentRepository;
+	
+	public StudentServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+	@Override
+	public List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
+
+	@Override
+	public Student saveStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public Student getStudentById(Long id) {
+		return studentRepository.findById(id).get();
+	}
+
+	@Override
+	public Student updateStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public void deleteStudentById(Long id) {
+		studentRepository.deleteById(id);	
+	}
+
+
+}
